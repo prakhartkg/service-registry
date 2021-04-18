b6fa95ce62db6627e4659fcb102241c0e0767800
Prakhar Jain <prakhartkg@gmail.com>
2021-04-19 04:20:39 +0530

    Initial Commit

---
+++ src/test/java/com/pinecone/service/registory/ServiceRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinecone.service.registory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
